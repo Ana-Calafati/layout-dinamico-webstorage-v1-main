742986a1ad65ff27e571b1c86ca67df37c5f3917
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-04-23 11:28:45 -0300

    nao esta linkado o css

---
+++ spa-fetch-components/components/menu.js
@@ -0,0 +1,30 @@
+export const iniciarMenu = () =>{
+    const menus = document.querySelectorAll('[data-menu]');
+    menus.forEach((menus) => {
+        const botao = menus.querySelector('.menu__botao');
+        const toggleMenu = () => {
+            menu.classList.toggle('active');
+            const ativo = menu.classList.contains('active');
+            botao.setAttribute('aria-expanded', ativo);
+            botao.setAttribute('aria-label', ativo ? 'Fechar Menu': 'Abrir Menu');
+        };
+
+        const fecharComEsc = (event) =>{
+            const isEsc = event.key === 'Escape';
+            const ativo = menu.classList.contains('active');
+
+            if (isEsc && ativo){
+                menu.classList.remove('active');
+                
+                botao.setAttribute('aria-expanded', false);
+                botao.setAttribute('aria-label', 'Abrir Menu');
+                botao.focus();
+            }
+
+        };
+
+        botao.addEventListener('pointerdown', toggleMenu);
+        document.addEventListener('keydown', fecharComEsc)
+    });
+};
+
