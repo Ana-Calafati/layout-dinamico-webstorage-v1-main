3b95796ea02a795729069d26896beeed54085b99
Ana Grifoni <ana.grifoni@aluno.senai.br>
2026-04-24 09:33:00 -0300

    manodo ceu

--- spa-fetch-components/js/main.js
+++ spa-fetch-components/js/main.js
@@ -1,58 +1,58 @@
 import { iniciarMenu } from "../components/menu.js";
 
+
+// Carrega HTML via fetch e injeta no container
 const carregarComponente = async (caminho, container) =>{
     try{
         const resposta = await fetch(caminho);
 
-        if(!resposta.ok){
+        if (!resposta.ok){
             throw new Error('Erro ao carregar componente')
         }
-        
-        const html = await resposta.text();
-        container.innerHTML =html;
-    }catch(erro){
-        console.error(erro);
 
+        const html = await resposta.text();
+        container.innerHTML = html;
+
+    }catch(erro){
+        console.error(erro)
     }
 
 };
 
-const iniciarLinks = () => {
+
+//Controla a navegação SPA
+const iniciarLinks = () =>{
     const conteudo = document.querySelector('[data-conteudo]');
-
-    document.querySelectorAll('[data-link]').forEach((Link) => {
-        Link.addEventListener('click', async (event)=>{
+    document.querySelectorAll('[data-link]').forEach((link) => {
+        link.addEventListener('click', async (event) =>{
             event.preventDefault();
-        const pagina = link.dataset.link;
-
+        const pagina = link.dataset.link
         const menu = document.querySelector('[data-menu]');
         const botao = document.querySelector('.menu__botao');
-
+        
         if (menu.classList.contains('active')){
             menu.classList.remove('active')
-
             botao.setAttribute('aria-expanded', false);
             botao.setAttribute('aria-label', 'Abrir Menu');
         }
 
-            await carregarComponente(`./pages/${pagina}.html`, conteudo)
+        await carregarComponente(`./pages/${pagina}.html`, conteudo)
 
         });
-
     });
-
 };
 
-const iniciarApp = async () =>{
+const iniciarApp = async () => {
+
 const containerMenu = document.querySelector('[data-componente="menu"]')
+
 await carregarComponente('./components/menu.html', containerMenu)
 
 iniciarMenu();
-
 iniciarLinks();
 
-const conteudo = document.querySelector('[data-conteudo]');
-await carregarComponente('./pages/home.html', conteudo);
+const conteudo = document.querySelector('[data-conteudo]')
+await carregarComponente('./pages/home.html', conteudo)
 
 };
 
